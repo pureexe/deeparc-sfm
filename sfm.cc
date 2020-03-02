e3d47c8ab77f8aa0492e6a33cd5abb546eb87ff9
Pakkapon Phongthawee <pure.gif@gmail.com>
2020-03-02 12:21:03 +0700

    update

--- sfm.cc
+++ sfm.cc
@@ -12,10 +12,10 @@ int main(int argc, char** argv) {
     // google::SetLogDestination(google::GLOG_INFO,"../log/" );
     DeepArcManager* deeparcManager = new DeepArcManager();
     double residuals[2];
-    deeparcManager->read("../assets/teabottle_green_no_share.deeparc");
-    deeparcManager->ply("../assets/teabottle_green_no_share_init.ply");
-    //deeparcManager->read("../assets/teabottle_green.deeparc");
-    //deeparcManager->ply("../assets/teabottle_green_init.ply");
+    //deeparcManager->read("../assets/teabottle_green_no_share.deeparc");
+    //deeparcManager->ply("../assets/teabottle_green_no_share_init.ply");
+    deeparcManager->read("../assets/teabottle_green.deeparc");
+    deeparcManager->ply("../assets/teabottle_green_init.ply");
     ceres::Problem problem;
     double *instrinsic,*extrinsic, *extrinsic_row, *extrinsic_col, *point3d;
     int row_id, col_id;
@@ -43,6 +43,8 @@ int main(int argc, char** argv) {
                 );
                 // cam_0,0 should be always identity
                 problem.SetParameterBlockConstant(extrinsic_row);
+                //Freeze Intrinsic and extrinsic
+                problem.SetParameterBlockConstant(instrinsic);
             }else if(row_id == 0){
                 problem.AddResidualBlock(cost_fn,
                     new ceres::CauchyLoss(0.5),
@@ -50,6 +52,7 @@ int main(int argc, char** argv) {
                     extrinsic_col,
                     point3d
                 );
+                
             }else if(col_id == 0){
                 problem.AddResidualBlock(cost_fn,
                     new ceres::CauchyLoss(0.5),
@@ -57,6 +60,9 @@ int main(int argc, char** argv) {
                     extrinsic_row,
                     point3d
                 );
+                //Freeze Intrinsic and extrinsic
+                problem.SetParameterBlockConstant(instrinsic);
+                problem.SetParameterBlockConstant(extrinsic_row);
             }else{
                 problem.AddResidualBlock(cost_fn,
                     new ceres::CauchyLoss(0.5),
@@ -65,6 +71,10 @@ int main(int argc, char** argv) {
                     extrinsic_col,
                     point3d
                 );
+                //Freeze Intrinsic and extrinsic
+                problem.SetParameterBlockConstant(instrinsic);
+                problem.SetParameterBlockConstant(extrinsic_row);
+                problem.SetParameterBlockConstant(extrinsic_col);
             }
         }
     }else{
@@ -91,18 +101,18 @@ int main(int argc, char** argv) {
     ceres::Solver::Options options;
     options.linear_solver_type = ceres::DENSE_SCHUR;
     options.minimizer_progress_to_stdout = true;
-    options.max_num_iterations = 1000 * 1; // 1000 iteration 
+    options.max_num_iterations = 100 * 1; // 1000 iteration 
     options.num_threads = 22; //use 20 thread
     options.max_solver_time_in_seconds = 3600 * 1; // 1 hour
     ceres::Solver::Summary summary;
     ceres::Solve(options, &problem, &summary);
     std::cout << summary.FullReport() << "\n";
-    //deeparcManager->ply("../assets/teabottle_green_noisy.ply");
-    deeparcManager->ply("../assets/teabottle_green_no_share_noisy.ply");
+    deeparcManager->ply("../assets/teabottle_green_noisy.ply");
+    //deeparcManager->ply("../assets/teabottle_green_no_share_noisy.ply");
     bool* mask = deeparcManager->point3d_mask(5);
     deeparcManager->point3d_remove(mask);
-    //deeparcManager->ply("../assets/teabottle_green_clear.ply");
-    //deeparcManager->write("../assets/teabottle_green_clear.deeparc");
-    deeparcManager->ply("../assets/teabottle_green_no_share_clear.ply");
-    deeparcManager->ply("../assets/teabottle_green_no_share_clear.deeparc");
+    deeparcManager->ply("../assets/teabottle_green_clear.ply");
+    deeparcManager->write("../assets/teabottle_green_clear.deeparc");
+    //deeparcManager->ply("../assets/teabottle_green_no_share_clear.ply");
+    //deeparcManager->ply("../assets/teabottle_green_no_share_clear.deeparc");
 }
